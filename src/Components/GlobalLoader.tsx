1f4818cf0680dc680010ce627fe78e46feddd7b1
David Stockton <dave@davidstockton.com>
2021-04-24 20:35:21 -0600

    Show number of fetches running in global loader spinner
    Closes #19

--- src/Components/GlobalLoader.tsx
+++ src/Components/GlobalLoader.tsx
@@ -5,17 +5,18 @@ import {useIsFetching} from "react-query";
 
 function GlobalLoader() {
   const isFetching = useIsFetching();
-
+  let hidden = false;
   if (!isFetching) {
-    return null;
+    hidden = true;
   }
 
   return (
-    <div className={'global-loader'}>
+    <div className={`global-loader ${hidden ? 'hidden' : ''}`}>
+      <span>{isFetching}</span>
       <Loader type={'ThreeDots'}
               height={100}
               width={100}
-              color={'#2074ba'}
+              color={'#eeeeee'}
       />
     </div>
   );
