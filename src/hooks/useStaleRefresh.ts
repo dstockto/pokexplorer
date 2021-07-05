c7e3fc1e945c305924e3ed413845ac38589aaea3
David Stockton <dave@davidstockton.com>
2021-07-04 21:09:41 -0600

    Add useStaleRefresh hook for testing later

---
+++ src/hooks/useStaleRefresh.ts
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+
+interface CacheInterface {
+  [key: string]: any,
+}
+
+const CACHE: CacheInterface = {};
+
+export default function useStaleRefresh(url: string, defaultValue: any
+  = []) {
+  const [data, setData] = useState(defaultValue);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // cacheID is how a cache is identified against a unique request
+    const cacheID = url;
+    // look in cache and set response if present
+    if (CACHE[cacheID] !== undefined) {
+      setData(CACHE[cacheID]);
+      setLoading(false);
+    } else {
+      // else make sure loading set to true
+      setLoading(true);
+      setData(defaultValue);
+    }
+    // fetch new data
+    fetch(url)
+      .then((res) => res.json())
+      .then((newData) => {
+        CACHE[cacheID] = newData;
+        setData(newData);
+        setLoading(false);
+      });
+  }, [url, defaultValue]);
+
+  return [data, isLoading];
+};
